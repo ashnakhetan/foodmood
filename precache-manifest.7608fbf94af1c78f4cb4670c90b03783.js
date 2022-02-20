2e4532bdbe1674f1ead246f9b9785a6c72f2f45c
ashnakhetan <ashnakhetan@gmail.com>
2022-02-20 09:01:00 -0800

    Updates

---
+++ precache-manifest.7608fbf94af1c78f4cb4670c90b03783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2feca436914b84540e3ebe07c135cf5",
+    "url": "/foodmood/index.html"
+  },
+  {
+    "revision": "d75af90c9dac0dd3861e",
+    "url": "/foodmood/static/css/main.669bf869.chunk.css"
+  },
+  {
+    "revision": "3bd0a528c278c81e5de0",
+    "url": "/foodmood/static/js/2.fb1a7df5.chunk.js"
+  },
+  {
+    "revision": "762dd207dbc1045a2888f97129ac73c2",
+    "url": "/foodmood/static/js/2.fb1a7df5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75af90c9dac0dd3861e",
+    "url": "/foodmood/static/js/main.bf375bab.chunk.js"
+  },
+  {
+    "revision": "c9a35585fd29ae4fba31",
+    "url": "/foodmood/static/js/runtime-main.3daef115.js"
+  }
+]);
